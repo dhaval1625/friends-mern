3f96c8284f2393fd9fdd0c72fad8af9a1f50485e
techerudite-dhaval-l <Dhaval@techerudite.com>
2024-04-25 13:52:01 +0530

    drag & drop file input done

--- frontend/src/components/sections/DPForm.tsx
+++ frontend/src/components/sections/DPForm.tsx
@@ -8,50 +8,25 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import TextSecondary from '../Typography/TextSecondary';
 import { BiUser } from 'react-icons/bi';
-import { cn } from '@/lib/utils';
+import { cn, fileHandler, uploadImageOnCloud } from '@/lib/utils';
 import { useState, useRef } from 'react';
-import imageCompression from 'browser-image-compression';
-import { v4 as uuid } from 'uuid';
-// import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-// import { storage } from '@/lib/firebase';
+import { Button } from '../ui/button';
+import { FaArrowRightLong } from 'react-icons/fa6';
+import { FileUploader } from 'react-drag-drop-files';
 
 function DPForm() {
-   const uploadDP = useRef<HTMLInputElement>(null);
-   // const [DP, setDP] = useState(null);
+   const fileInputRef = useRef<HTMLInputElement>(null);
    const [dpURL, setDpURL] = useState<null | string>(null);
-   const [fileURL, setFileURL] = useState<string>('');
+   const [uploadedImg, setUploadedImg] = useState<string>('');
 
-   function uploadImageHandler() {
-      uploadDP.current?.click();
+   async function handleImageUpload(file: File) {
+      // display uploaded image on avatar
+      fileHandler(file, (url: string) => setUploadedImg(url));
+
+      // get url of uploaded image
+      const url = await uploadImageOnCloud(file);
+      setDpURL(url);
    }
-
-   function displayImageHandler(file: File) {
-      const fileReader = new FileReader();
-      fileReader.onload = (e) => {
-         const result = e.target!.result as string;
-         if (result) setFileURL(result);
-      };
-
-      fileReader.readAsDataURL(file);
-
-      uploadImage(file);
-   }
-
-   const uploadImage = async function (imageFile: File) {
-      const options = {
-         maxSizeMB: 0.3,
-         maxWidthOrHeight: 1920,
-         useWebWorker: true,
-      };
-      const compressedImage = await imageCompression(imageFile, options);
-
-      // const path = ref(storage, `dp-images/${uuid()}-${imageFile.name}`);
-
-      // await uploadBytes(path, compressedImage);
-      // const url = await getDownloadURL(path);
-      // console.log('image uploaded url', url);
-      // setDpURL(url);
-   };
 
    return (
       <Card className="max-w-md w-full">
@@ -63,35 +38,46 @@ function DPForm() {
             </CardDescription>
          </CardHeader>
          <CardContent>
-            <div
-               onClick={uploadImageHandler}
-               className={cn(
-                  'border border-neutral-700/40 border-dashed',
-                  'rounded-md p-4 flex items-center space-x-5',
-                  'cursor-pointer hover:border-neutral-700'
-               )}
+            <FileUploader
+               handleChange={handleImageUpload}
+               name="profile-picture"
+               dropMessageStyle={{color: 'white', backgroundColor: '#4040409c', fontWeight: 700, fontSize: '1.2rem', opacity: 1}}
             >
-               <Avatar size="2xl">
-                  <AvatarImage src={fileURL} />
-                  <AvatarFallback className="text-neutral-500">
-                     <BiUser size="3rem" />
-                  </AvatarFallback>
-               </Avatar>
-               <TextSecondary className="max-w-[200px]">
-                  Drag and drop an image or browse on your device
-               </TextSecondary>
-            </div>
+               <div
+                  className={cn(
+                     'border border-neutral-700/40 border-dashed',
+                     'rounded-md p-4 flex items-center space-x-5',
+                     'cursor-pointer hover:border-neutral-700'
+                  )}
+               >
+                  <Avatar size="2xl">
+                     <AvatarImage src={uploadedImg} alt="uploaded image" />
+                     <AvatarFallback className="text-neutral-500">
+                        <BiUser size="3rem" />
+                     </AvatarFallback>
+                  </Avatar>
+                  <TextSecondary className="max-w-[200px]">
+                     Drag and drop an image or browse on your device
+                  </TextSecondary>
+               </div>
+            </FileUploader>
             <input
                onChange={(e) =>
-                  e.target.files && displayImageHandler(e.target.files[0])
+                  e.target.files && handleImageUpload(e.target.files[0])
                }
-               ref={uploadDP}
+               ref={fileInputRef}
                type="file"
                id="profile-picture"
-               name="profile-picture"
+               className="hidden"
                hidden
                accept="image/*"
             />
+            <div className="flex justify-between items-center mt-4">
+               <Button>Continue</Button>
+               <Button variant="ghost">
+                  <span className="mr-2">Skip</span> <FaArrowRightLong />
+               </Button>
+            </div>
          </CardContent>
       </Card>
    );
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,33 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import imageCompression from 'browser-image-compression';
+import { v4 as uuid } from 'uuid';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { storage } from '@/lib/firebase';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+   return twMerge(clsx(inputs));
 }
+
+export const uploadImageOnCloud = async function (imageFile: File) {
+   const options = {
+      maxSizeMB: 0.3,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+   };
+   const compressedImage = await imageCompression(imageFile, options);
+   const path = ref(storage, `friends-images/${uuid()}-${imageFile.name}`);
+
+   await uploadBytes(path, compressedImage);
+   const url = await getDownloadURL(path);
+   return url;
+};
+
+export const fileHandler = (file: File, cb: Function) => {
+   const fileReader = new FileReader();
+   fileReader.onload = (e) => {
+      const result = e.target!.result as string;
+      if (result) cb(result);
+   };
+   fileReader.readAsDataURL(file);
+};
